ec054d8baba4dba8de4e52494d68e4a1de4876cb
gitdenlev <levchenkoden02@gmail.com>
2023-03-04 18:45:21 +0200

    Calculator

---
+++ calculator.js
@@ -0,0 +1,24 @@
+const calc = document.querySelector(".calculator");
+
+const result = document.querySelector("p");
+
+calc.addEventListener("click", function (event) {
+  if (!event.target.classList.contains("js-calc-btn")) return;
+  const value = event.target.innerText;
+  switch (value) {
+    case "=":
+      result.innerText = eval(result.innerText);
+      break;
+    case "AC":
+      result.innerText = "";
+      break;
+    case "%":
+      result.innerText = result.innerText * 0.01;
+      break;
+    case "+/-":
+      result.innerText = "-";
+      break;
+    default:
+      result.innerText += value;
+  }
+});
